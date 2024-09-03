8fb266c7913841fa90d0c3e16e54a2f23da00ef0
Alexandr Glazunov <aglazunov@rutube.ru>
2024-09-03 22:42:59 +0300

    chore: update kitty + go

--- dot_config/nvim/lua/plugins/go.lua
+++ dot_config/nvim/lua/plugins/go.lua
@@ -1,20 +1,36 @@
 return {
-  -- {
-  --   "williamboman/mason.nvim",
-  --   opts = function(_, opts)
-  --     opts.ensure_installed = opts.ensure_installed or {}
-  --     vim.list_extend(opts.ensure_installed, { "goimports-reviser" })
-  --   end,
-  -- },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "gci" })
+    end,
+  },
   {
     "stevearc/conform.nvim",
     opts = function(_, opts)
       -- formattag should be the first source as it adds extra newline at the
       -- end of the file, thus breaking gofumpt linting
-      opts.formatters_by_ft.go = { "gofumpt", "formattag", "goimports" }
+      opts.formatters_by_ft.go = { "gofumpt", "formattag", "goimports", "gci" }
       opts.formatters.formattag = {
         command = "formattag",
         stdin = true,
+      }
+      opts.formatters.gci = {
+        command = "gci",
+        stdin = true,
+        args = {
+          "print",
+          "--custom-order",
+          "--section",
+          "standard",
+          "--section",
+          "localmodule",
+          "--section",
+          "prefix(gitlab.rutube.ru)",
+          "--section",
+          "default",
+        },
       }
     end,
   },
