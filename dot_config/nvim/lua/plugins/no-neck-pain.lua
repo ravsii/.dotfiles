8b178ceba9ff110b98cf2523c52d032456d75fc8
Alexandr Glazunov <aglazunov@rutube.ru>
2024-09-26 19:46:13 +0300

    nvim: disable nnp by default

--- dot_config/nvim/lua/plugins/no-neck-pain.lua
+++ dot_config/nvim/lua/plugins/no-neck-pain.lua
@@ -6,7 +6,7 @@ return {
       opts = {
         debug = false,
         width = 90,
-        -- minSideBufferWidth = 10,
+        minSideBufferWidth = 10,
         disableOnLastBuffer = false,
         -- When `true`, disabling the plugin closes every other windows except the initially focused one.
         --- @type boolean
@@ -16,7 +16,7 @@ return {
           -- If the main window is  a side tree (e.g. NvimTree) or a dashboard, the command is delayed until it finds a valid window.
           -- The command is cleaned once it has successfuly ran once.
           --- @type boolean
-          enableOnVimEnter = true,
+          enableOnVimEnter = false,
           -- When `true`, enables the plugin when you enter a new Tab.
           -- note: it does not trigger if you come back to an existing tab, to prevent unwanted interfer with user's decisions.
           --- @type boolean
