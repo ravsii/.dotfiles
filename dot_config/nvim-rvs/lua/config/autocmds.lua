f33277985b6dba35b5101949d31cffb9def93866
Alex <naplle58@gmail.com>
2024-11-03 16:34:25 +0300

    remove: my nvim config

--- dot_config/nvim-rvs/lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
--- Highlight on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank({
-      higroup = "IncSearch", -- The highlight group to use, customize as desired
-      timeout = 1000, -- Duration of highlight in milliseconds
-    })
-  end,
-})
--- dot_config/nvim-rvs/lua/plugins/lsp.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPost", "BufNewFile" },
-  cmd = { "LspInfo", "LspInstall", "LspUninstall" },
-  dependencies = {
-    "mason.nvim",
-    { "williamboman/mason-lspconfig.nvim", config = function() end },
-  },
-}
