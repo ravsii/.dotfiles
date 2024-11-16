47245c7cb87d821a4d931ddf420af38326ff7e15
Alex <naplle58@gmail.com>
2024-11-16 22:02:24 +0300

    nvim: tmp: comment dap
    its broken

--- dot_config/nvim/lua/plugins/dap.lua
+++ dot_config/nvim/lua/plugins/dap.lua
@@ -1,45 +1,43 @@
 return {
-  {
-    "rcarriga/nvim-dap-ui",
-    opts = {
-      layouts = {
-        {
-          elements = {
-            {
-              id = "scopes",
-              size = 0.7,
-            },
-            {
-              id = "breakpoints",
-              size = 0.2,
-            },
-            {
-              id = "stacks",
-              size = 0.1,
-            },
-          },
-          position = "left",
-          size = 40,
-        },
-        {
-          elements = {
-            {
-              id = "repl",
-              size = 1,
-            },
-          },
-          position = "bottom",
-          size = 20,
-        },
-      },
-      mappings = {
-        edit = "e",
-        expand = { "<CR>", "<2-LeftMouse>" },
-        open = "o",
-        remove = "d",
-        repl = "r",
-        toggle = "t",
-      },
-    },
-  },
+  "rcarriga/nvim-dap-ui",
+  -- opts = {
+  --   layouts = {
+  --     {
+  --       elements = {
+  --         {
+  --           id = "scopes",
+  --           size = 0.7,
+  --         },
+  --         {
+  --           id = "breakpoints",
+  --           size = 0.2,
+  --         },
+  --         {
+  --           id = "stacks",
+  --           size = 0.1,
+  --         },
+  --       },
+  --       position = "left",
+  --       size = 40,
+  --     },
+  --     {
+  --       elements = {
+  --         {
+  --           id = "repl",
+  --           size = 1,
+  --         },
+  --       },
+  --       position = "bottom",
+  --       size = 15,
+  --     },
+  --   },
+  --   mappings = {
+  --     edit = "e",
+  --     expand = { "<CR>", "<2-LeftMouse>" },
+  --     open = "o",
+  --     remove = "d",
+  --     repl = "r",
+  --     toggle = "t",
+  --   },
+  -- },
 }
