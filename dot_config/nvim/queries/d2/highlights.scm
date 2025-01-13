0424436e2923ebcb3a8e68cafd9a22e54e7ee241
Alex <naplle58@gmail.com>
2025-01-13 22:16:36 +0300

    chore(nvim): update d2 queries

--- dot_config/nvim/queries/d2/highlights.scm
+++ dot_config/nvim/queries/d2/highlights.scm
@@ -1,4 +1,4 @@
-(comment) @comment
+((comment) @comment @spell)
 
 [
  (label)
@@ -10,13 +10,14 @@
 (identifier) @variable
 (identifier
   (identifier) @variable.member)
-; TODO: Move them to the parser
+
 ((identifier) @constant.builtin
  (#match? @constant.builtin "^(bold|border-radius|direction|double-border|fill|fill-pattern|filled|font|font-color|font-size|italic|label|multiple|opacity|shadow|shape|source-arrowhead|stroke|stroke-dash|stroke-width|style|target-arrowhead|text-transform|underline)$"))
 
 (connection) @operator
 (integer) @number
-(bool) @constant.builtin
+(float) @number.float
+(boolean) @boolean
 
 (import) @module
 
@@ -30,5 +31,13 @@
   "{"
   "}"
   "|"
+  "||"
+  "|||"
+  "|`"
+  "`|"
 ] @punctuation.bracket
-"." @punctuation.delimiter
+[
+  "."
+  ";"
+  ":"
+] @punctuation.delimiter
