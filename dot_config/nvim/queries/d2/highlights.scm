06a3d63eb22341bcc4784c316b2587dc5ed1f4e1
Alex <naplle58@gmail.com>
2025-01-14 22:58:52 +0300

    chore: hl

--- dot_config/nvim/queries/d2/highlights.scm
+++ dot_config/nvim/queries/d2/highlights.scm
@@ -1,20 +1,72 @@
 ((comment) @comment @spell)
 
 [
- (label)
- (label_codeblock)
+  (label)
+  (label_codeblock)
+  (label_constraint)
 ] @string
 
 (escape_sequence) @string.escape
 
-(identifier) @variable
-(identifier
-  (identifier) @variable.member)
+((label_constraint) @constant
+  (#any-of? @constant
+    "primary_key"
+    "foreign_key"
+    "unique"
+    "PK"
+    "FK"
+    "UNQ"
+  )
+)
 
-((identifier) @constant.builtin
- (#match? @constant.builtin "^(bold|border-radius|direction|double-border|fill|fill-pattern|filled|font|font-color|font-size|italic|label|multiple|opacity|shadow|shape|source-arrowhead|stroke|stroke-dash|stroke-width|style|target-arrowhead|text-transform|underline)$"))
+
+(identifier) @function
+((identifier) @function.builtin
+  (#any-of? @function.builtin
+    "3d"
+    "animated"
+    "bold"
+    "border-radius"
+    "class"
+    "classes"
+    "constraint"
+    "d2-config"
+    "direction"
+    "double-border"
+    "fill"
+    "fill-pattern"
+    "filled"
+    "font"
+    "font-color"
+    "font-size"
+    "height"
+    "italic"
+    "label"
+    "layers"
+    "link"
+    "multiple"
+    "near"
+    "opacity"
+    "scenarios"
+    "shadow"
+    "shape"
+    "source-arrowhead"
+    "steps"
+    "stroke"
+    "stroke-dash"
+    "stroke-width"
+    "style"
+    "target-arrowhead"
+    "text-transform"
+    "tooltip"
+    "underline"
+    "vars"
+    "width"
+  )
+)
 
 (connection) @operator
+(connection_identifier) @property
 (integer) @number
 (float) @number.float
 (boolean) @boolean
@@ -28,6 +80,8 @@
 [
   "["
   "]"
+  "("
+  ")"
   "{"
   "}"
   "|"
