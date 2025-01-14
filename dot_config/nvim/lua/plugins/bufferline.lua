e79b8a9c71eb4c401c06f82caf2b97abc9251598
Alex <naplle58@gmail.com>
2025-01-14 12:05:07 +0300

    revert(nvim): bufferline fix
    it's a bug, fix here https://github.com/akinsho/bufferline.nvim/pull/991

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -2,13 +2,12 @@ return {
   "akinsho/bufferline.nvim",
   opts = function(_, opts)
     local bufferline = require("bufferline")
+
     opts.options.mode = "buffers"
     opts.options.style_preset = bufferline.style_preset.default
     opts.options.separator_style = "thick"
     opts.options.always_show_bufferline = true
     opts.options.show_buffer_close_icons = false
-    opts.color_icons = true
-    opts.show_buffer_icons = true
     opts.options.tab_size = 1 -- As min as possible
 
     return opts
