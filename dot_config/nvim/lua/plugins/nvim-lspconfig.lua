da682e7aa29f0ec5c1a70550e730fbec79a472e9
Alex <naplle58@gmail.com>
2025-05-16 08:45:54 +0300

    fix(nvim): golangci lsp config

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     -- golangci-lint pre 2.0
-    commit = "200280dab91a7df2ca42fd2ab3cb4c5fab056666",
+    -- commit = "200280dab91a7df2ca42fd2ab3cb4c5fab056666",
   },
 }
