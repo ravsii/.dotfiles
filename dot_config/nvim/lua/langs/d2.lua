3433ab242606ecf812a1692ae70617811bb800ed
Alex <naplle58@gmail.com>
2025-05-27 01:17:48 +0300

    chore(nvim): migrate to nvim-treesitter's main branch

--- dot_config/nvim/lua/langs/d2.lua
+++ dot_config/nvim/lua/langs/d2.lua
@@ -1,8 +1,11 @@
 return {
   {
     "ravsii/tree-sitter-d2",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    dependencies = { "nvim-treesitter/nvim-treesitter", branch = "main" },
+    -- dir = "~/dev/OpenSource/tree-sitter-d2/",
+    -- ft = "d2",
     build = "make nvim-install",
-    branch = "v0.7.1",
+    branch = "nvim-treesitter-main",
+    -- tag = "v0.7.1",
   },
 }
