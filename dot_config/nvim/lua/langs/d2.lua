88b9266d36e24f17544abfe3b7b3b02fd31ac41f
Alex <naplle58@gmail.com>
2025-05-27 06:36:04 +0300

    fix(nvim): nvim-treesitter branch

--- dot_config/nvim/lua/langs/d2.lua
+++ dot_config/nvim/lua/langs/d2.lua
@@ -1,11 +1,7 @@
 return {
   {
     "ravsii/tree-sitter-d2",
-    dependencies = { "nvim-treesitter/nvim-treesitter", branch = "main" },
-    -- dir = "~/dev/OpenSource/tree-sitter-d2/",
-    -- ft = "d2",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
     build = "make nvim-install",
-    branch = "nvim-treesitter-main",
-    -- tag = "v0.7.1",
   },
 }
--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
-  branch = "main",
+  branch = "master",
   build = ":TSUpdate",
 }
