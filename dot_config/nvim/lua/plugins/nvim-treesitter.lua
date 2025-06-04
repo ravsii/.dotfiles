3679be852a3f7509d1f344a475228226fc2bcc66
Alex <naplle58@gmail.com>
2025-06-04 22:00:25 +0300

    feat(nvim): add args swap

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,4 +3,17 @@ return {
   lazy = false,
   branch = "master",
   build = ":TSUpdate",
+  opts = {
+    textobjects = {
+      swap = {
+        enable = true,
+        swap_previous = {
+          ["H"] = "@parameter.inner",
+        },
+        swap_next = {
+          ["L"] = "@parameter.inner",
+        },
+      },
+    },
+  },
 }
