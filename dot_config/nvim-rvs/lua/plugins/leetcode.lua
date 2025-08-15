f9cbb24a807dd6a33d293a7b3e22bab2c7edc129
Alex <naplle58@gmail.com>
2025-08-15 23:12:36 +0300

    fix(nvim): disable diagnostics for leetcode

--- dot_config/nvim-rvs/lua/plugins/leetcode.lua
+++ dot_config/nvim-rvs/lua/plugins/leetcode.lua
@@ -36,6 +36,7 @@ return {
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "leetcode.nvim",
         callback = function()
+          vim.diagnostic.enable(false)
           vim.opt_local.wrap = true
         end,
       })
