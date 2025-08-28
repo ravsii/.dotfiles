1a1a7dc94dbb0f048adc231e901247277e948500
Alex <naplle58@gmail.com>
2025-08-29 00:19:19 +0300

    feat(nvim): timer keymaps

--- dot_config/nvim-rvs/lua/core/timer.lua
+++ dot_config/nvim-rvs/lua/core/timer.lua
@@ -1,18 +1,42 @@
 return {
   {
     "ravsii/timer.nvim",
-    -- dir = "~/dev/OpenSource/timer.nvim/",
-    -- dev = true,
-    opts = {},
-    -- init = function(_, opts)
-    --   local module = "timer"
-    --   vim.api.nvim_create_autocmd("BufWritePost", {
-    --     pattern = vim.fn.expand("~") .. "/dev/OpenSource/timer.nvim/**/*.lua",
-    --     callback = function()
-    --       package.loaded[module] = nil
-    --       require(module).setup(opts)
-    --     end,
-    --   })
-    -- end,
+    dir = "~/dev/OpenSource/timer.nvim/",
+    dev = true,
+    keys = {
+      { "<leader>T", "", desc = "+timers" },
+      {
+        "<leader>Tp",
+        function()
+          local t = require("timer.timer")
+          local d = require("timer.duration")
+          local u = require("timer.unit")
+          local m = require("timer")
+
+          local break_timer = t.new(d.from(5 * u.MINUTE), "Break is over")
+          local pomodoro_timer = t.new(
+            d.from(25 * u.MINUTE),
+            "Pomodoro is over",
+            function() m.start_timer(break_timer) end
+          )
+
+          m.start_timer(pomodoro_timer)
+        end,
+        desc = "Start Pomodoro 25/5 timer",
+      },
+    },
+    ---@module "timer.config"
+    ---@type Config
+    opts = {
+      persistent = true,
+    },
+    init = function()
+      local aug = vim.api.nvim_create_augroup("TimerDevReload", { clear = true })
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        group = aug,
+        pattern = vim.fn.expand("~") .. "/dev/OpenSource/timer.nvim/**/*.lua",
+        callback = function() vim.cmd("Lazy reload timer.nvim") end,
+      })
+    end,
   },
 }
