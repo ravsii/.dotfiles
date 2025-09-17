be5bbc7e62bd581f6f8b6bdce631c37365c25b82
Alex <naplle58@gmail.com>
2025-09-17 18:42:31 +0300

    nvim(lc): enable diagnostics by default

--- dot_config/nvim/lua/plugins/leetcode.lua
+++ dot_config/nvim/lua/plugins/leetcode.lua
@@ -35,10 +35,7 @@ return {
       -- Enable wrap for question window
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "leetcode.nvim",
-        callback = function()
-          vim.diagnostic.enable(false)
-          vim.opt_local.wrap = true
-        end,
+        callback = function() vim.opt_local.wrap = true end,
       })
 
       --- @module 'leetcode'
