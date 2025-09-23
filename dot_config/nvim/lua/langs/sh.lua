fd43981352fc6b6f3459141bd6609807077968b2
Alex <naplle58@gmail.com>
2025-09-23 14:42:10 +0300

    nvim(formatting): add shfmt to conform

--- dot_config/nvim/lua/langs/sh.lua
+++ dot_config/nvim/lua/langs/sh.lua
@@ -1,3 +1,8 @@
 require("install"):add_mason({ "shfmt" })
 
-return {}
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts) opts.formatters_by_ft.sh = { "shfmt" } end,
+  },
+}
