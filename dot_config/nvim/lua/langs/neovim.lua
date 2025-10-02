17ebde38f470d6d1e82bf50e8f268f4dbf555b49
Alex <naplle58@gmail.com>
2025-10-02 09:36:53 +0300

    add neovim testing stuff

--- dot_config/nvim/lua/langs/neovim.lua
+++ dot_config/nvim/lua/langs/neovim.lua
@@ -1,11 +1,14 @@
 return {
+  { "LuaCATS/luassert", name = "luassert-types", lazy = true },
+  { "LuaCATS/busted", name = "busted-types", lazy = true },
   {
     "folke/lazydev.nvim",
     ft = "lua",
-    cmd = "LazyDev",
     opts = {
       library = {
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "busted-types/library", words = { "describe" } },
+        { path = "luassert-types/library", words = { "assert" } },
         { path = "snacks.nvim", words = { "Snacks" } },
       },
     },
