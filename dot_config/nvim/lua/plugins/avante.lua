804921ba6fb564bc4894561e00ddf6f02788309e
Alex <naplle58@gmail.com>
2025-10-03 16:28:16 +0300

    disabled avante

--- dot_config/nvim/lua/plugins/avante.lua
+++ dot_config/nvim/lua/plugins/avante.lua
@@ -1,5 +1,9 @@
 return {
   {
+    -- I leave the configuration here, but with all the major AI models blocked
+    -- in russia, I don't wanna spin up VPN just to ask some stupid stuff.
+    enabled = false,
+
     "yetone/avante.nvim",
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     -- ⚠️ must add this setting! ! !
@@ -10,26 +14,6 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "folke/snacks.nvim", -- for input provider snacks
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
       {
         -- Make sure to set this up properly if you have lazy=true
         "MeanderingProgrammer/render-markdown.nvim",
@@ -48,7 +32,7 @@ return {
     ---@type avante.Config
     opts = {
       instructions_file = "avante.md",
-      provider = "ollama",
+      provider = "gemini",
       providers = {
         ollama = {
           endpoint = "http://localhost:11434",
