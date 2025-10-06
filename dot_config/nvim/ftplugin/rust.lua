1e28c79f8d851a23ffd2b020a8c091f7378a8422
Alex <naplle58@gmail.com>
2025-10-06 22:13:00 +0300

    revert some stuff

--- dot_config/nvim/ftplugin/rust.lua
+++ dot_config/nvim/ftplugin/rust.lua
@@ -1,5 +1,6 @@
 local opt = vim.opt
 
 opt.colorcolumn = "80,100"
-opt.tabstop = 79
+opt.tabstop = 4
 opt.shiftwidth = 4
+opt.textwidth = 79
--- dot_config/nvim/lua/core/colorscheme.lua
+++ dot_config/nvim/lua/core/colorscheme.lua
@@ -1,4 +1,26 @@
+-- set the theme in lazy.lua
+
 return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    ---@module "tokyonight"
+    ---@class tokyonight.Config
+    opts = {
+      style = "storm",
+      light_style = "day", -- The theme is used when the background is set to light
+      transparent = true,
+      terminal_colors = true,
+      styles = {
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "transparent", -- style for sidebars, see below
+        floats = "transparent", -- style for floating windows
+      },
+      dim_inactive = true, -- dims inactive windows
+      lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+    },
+  },
   {
     "rose-pine/neovim",
     name = "rose-pine",
@@ -9,4 +31,19 @@ return {
     },
     init = function() vim.cmd([[colorscheme rose-pine]]) end,
   },
+  {
+    "Mofiqul/vscode.nvim",
+    opts = {
+      transparent = true,
+      italic_comments = true,
+      italic_inlayhints = true,
+      underline_links = true,
+      disable_nvimtree_bg = true,
+      terminal_colors = true,
+    },
+  },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "rebelot/kanagawa.nvim" },
+  { "scottmckendry/cyberdream.nvim" },
+  { "thesimonho/kanagawa-paper.nvim" },
 }
