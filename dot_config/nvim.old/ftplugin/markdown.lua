0175e109bc4b5f44197adbc6b0f96095a23ce2f6
Alex <naplle58@gmail.com>
2026-02-08 18:04:05 +0300

    nvim.old: remove all

--- dot_config/nvim.old/ftplugin/markdown.lua
+++
@@ -1,5 +0,0 @@
-local opt = vim.opt
-
-opt.colorcolumn = "80"
-opt.textwidth = 79
-opt.sw = 2
--- dot_config/nvim.old/ftplugin/rust.lua
+++
@@ -1,5 +0,0 @@
-local opt = vim.opt
-
-opt.colorcolumn = "80,100"
-opt.tabstop = 79
-opt.shiftwidth = 4
