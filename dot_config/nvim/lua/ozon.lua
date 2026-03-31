24d33c452c127ef4e5a6476192e37bda054f2d5a
Alex <naplle58@gmail.com>
2026-03-31 12:38:13 +0300

    ozon: set sw, ts of yaml to 4

--- dot_config/nvim/lua/ozon.lua
+++ dot_config/nvim/lua/ozon.lua
@@ -4,6 +4,14 @@ local root = vim.fs.root(0, { ".golangci.pipeline.yaml" })
 if root then
   table.insert(vim.lsp.config.golangci_lint_ls.init_options.command, "--config=" .. root .. "/.golangci.pipeline.yaml")
 
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "yaml",
+    callback = function()
+      vim.bo.shiftwidth = 4
+      vim.bo.tabstop = 4
+    end,
+  })
+
   return {
     {
       "stevearc/conform.nvim",
